9b6cb8edd08b09674cc41f4afa29604ea2197dd9
JamesBond <introbond.dev@gmail.com>
2023-04-14 08:01:54 +0700

    feature: certification slide show keydown esc && create modal

--- src/pages/CertificationDisplay.jsx
+++ src/pages/CertificationDisplay.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { CertificationList } from "../apis/CertificationList";
 import "../styles/CertificationDisplay.css";
-import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai"
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import { Modal, Box } from '@mui/material';
 
 const CertificationDisplay = () => {
-    let { id } = useParams();
-    const [current, setCurrent] = useState(id)
+    const { id } = useParams();
+    const navigate = useNavigate(); // useNavigate hook from react-router-dom
+    const [current, setCurrent] = useState(id);
 
     useEffect(() => {
         const handleKeyDown = (event) => {
@@ -14,6 +16,8 @@ const CertificationDisplay = () => {
                 slideBack();
             } else if (event.keyCode === 39) {
                 slideForward();
+            } else if (event.keyCode === 27) {
+                closeModal();
             }
         };
         document.addEventListener("keydown", handleKeyDown);
@@ -24,35 +28,50 @@ const CertificationDisplay = () => {
 
     const slideBack = () => {
         if (current === 0) {
-            setCurrent(CertificationList.length - 1)
+            setCurrent(CertificationList.length - 1);
         } else {
-            setCurrent(parseInt(current) - 1)
+            setCurrent(parseInt(current) - 1);
         }
-    }
+    };
+
     const slideForward = () => {
         if (current === CertificationList.length - 1) {
-            setCurrent(0)
+            setCurrent(0);
         } else {
-            setCurrent(parseInt(current) + 1)
+            setCurrent(parseInt(current) + 1);
         }
+    };
+
+    const closeModal = () => {
+        navigate('/certifications');
     }
-    id = current;
 
     return (
-        <div className="certification-display" >
-            <Modal
-                isOpen={modalIsOpen}
-                onAfterOpen={afterOpenModal}
-                onRequestClose={closeModal}
-                style={customStyles}
-                contentLabel="Example Modal"
-            >
-                <AiOutlineArrowLeft className="arrow-left" onClick={slideBack} />
-                <img src={CertificationList[id].image} alt={CertificationList[id].name} />
-                <AiOutlineArrowRight className="arrow-right" onClick={slideForward} />
+        <div className="certification-display">
+            <Modal open={true} onClose={closeModal}>
+                <Box
+                    sx={{
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        bgcolor: 'background.paper',
+                        boxShadow: 24,
+                        p: 4,
+                        display: 'flex',
+                        gap: '1rem',
+                        flexDirection: 'column',
+                        width: '50vw',
+                        height: 'auto',
+                    }}
+                >
+                    <AiOutlineArrowLeft className="arrow-left" onClick={slideBack} />
+                    <img src={CertificationList[current].image} alt={CertificationList[current].name} />
+                    <AiOutlineArrowRight className="arrow-right" onClick={slideForward} />
+                </Box>
             </Modal>
         </div>
-    )
-}
+    );
+};
 
 export default CertificationDisplay;
