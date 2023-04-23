12c4a304f646a8f16bce2d29f837897a73020f33
JamesBond <introbond.dev@gmail.com>
2023-04-23 12:49:23 +0700

    refactor & clean code

--- src/components/Certification.jsx
+++ src/components/Certification.jsx
@@ -3,15 +3,13 @@ import { useNavigate } from "react-router-dom";
 
 const Certification = ({ ...certification }) => {
     const navigate = useNavigate();
-
     const disableClick = window.matchMedia("(max-width: 600px)").matches;
-
     const handleClick = () => {
         if (!disableClick) {
             navigate("/certification/" + certification.id);
-        }
-    }
-
+        };
+    };
+    
     return (
         <div className="certification" onClick={handleClick}>
             <div style={{ backgroundImage: `url(${certification.image})` }} className="bgImage" />
