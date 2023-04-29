928ed85de54e601f4a82dd445114644d26ec6811
JamesBond <introbond.dev@gmail.com>
2023-04-29 17:22:53 +0700

    feature: caching logic

--- backend/service/redis.go
+++ backend/service/redis.go
@@ -2,15 +2,21 @@ package service
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 )
 
-func NewRedisClient() *redis.Client {
-	redisHost := os.Getenv("REDIS_HOST")
+type RedisCache struct {
+	client *redis.Client
+}
 
+func NewRedisCache() RedisCache {
+	redisHost := os.Getenv("REDIS_HOST")
 	address := "localhost:6379"
 	if redisHost != "" {
 		address = redisHost
@@ -29,5 +35,42 @@ func NewRedisClient() *redis.Client {
 
 	fmt.Println("Connected to Redis:", pong)
 
-	return client
+	return RedisCache{client}
+}
+
+func (r RedisCache) GetCache(key string, data interface{}) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	result, err := r.client.Get(ctx, key).Result()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			return errors.New("cache miss")
+		}
+		return err
+	}
+
+	err = json.Unmarshal([]byte(result), &data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r RedisCache) SetCache(key string, data interface{}, expiration time.Duration) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	err = r.client.Set(ctx, key, jsonData, expiration).Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
