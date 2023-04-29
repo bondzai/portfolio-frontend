5ab9fd92923d5ee41780544f7042194c74cd5209
JamesBond <introbond.dev@gmail.com>
2023-04-29 14:01:06 +0700

    feature: redis

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,27 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"context"
+	"fmt"
+
+	service "portfolio/service"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 
+	redisClient := service.NewRedisClient()
+
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		pong, err := redisClient.Ping(context.Background()).Result()
+		if err != nil {
+			fmt.Println("Error pinging Redis:", err)
+			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+
+		return c.SendString("Hello, World! Redis ping result: " + pong)
 	})
 
 	app.Listen(":5000")
