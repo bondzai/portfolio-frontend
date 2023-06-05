f0c5803396f44f2bed3eaacc295c6a52c99f03de
JamesBond <introbond.dev@gmail.com>
2023-06-05 12:04:12 +0700

    Update: backend endpoint meshing

--- frontend/src/apis/CertificationList.js
+++ frontend/src/apis/CertificationList.js
@@ -1,13 +1,20 @@
 import axios from 'axios';
 
 const getCertificationList = (async () => {
-    const BACKEND_URL = import.meta.env.VITE_BACKEND_URL + "/certifications/";
-    try {
-        const response = await axios.get(BACKEND_URL);
-        return response.data.sort((a, b) => b.id - a.id);
-    } catch (error) {
-        console.error(error);
-    };
+    const BACKEND_URLS = [
+        import.meta.env.VITE_BACKEND_URL1,
+        import.meta.env.VITE_BACKEND_URL2,
+    ].map(url => url + "/certifications/");
+
+    for (let url of BACKEND_URLS) {
+        try {
+            const response = await axios.get(url);
+            return response.data.sort((a, b) => b.id - a.id);
+        } catch (error) {
+            console.error(`Error with URL ${url}: ${error}`);
+        }
+    }
+    throw new Error('All backend services are unavailable.');
 });
 
 export { getCertificationList }
