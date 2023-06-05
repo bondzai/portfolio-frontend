e7ad5b0e39a802832f2b08ece60a294afda97120
JamesBond <introbond.dev@gmail.com>
2023-06-05 09:04:27 +0700

    rearrange skill type

--- frontend/src/pages/Skills.jsx
+++ frontend/src/pages/Skills.jsx
@@ -7,12 +7,12 @@ const Skills = () => {
         <div className="home">
             <div className="skills">  
                 <ol className="list">
-                    <li> <b> Programming Languages </b> <SkillsFilter topic = "language"/> </li>
-                    <li> <b> Frontend </b> <SkillsFilter topic = "frontend"/> </li>
-                    <li> <b> Backend </b> <SkillsFilter topic = "backend"/> </li>
-                    <li> <b> Databases </b> <SkillsFilter topic = "database"/> </li>
-                    <li> <b> DevOps & Other Tools </b> <SkillsFilter topic = "tools"/> </li>
                     <li> <b> OS </b> <SkillsFilter topic = "os"/> </li>
+                    <li> <b> Programming Languages </b> <SkillsFilter topic = "language"/> </li>
+                    <li> <b> DevSecOps & Others </b> <SkillsFilter topic = "tools"/> </li>
+                    <li> <b> Backend </b> <SkillsFilter topic = "backend"/> </li>
+                    <li> <b> Frontend </b> <SkillsFilter topic = "frontend"/> </li>
+                    <li> <b> Databases </b> <SkillsFilter topic = "database"/> </li>
                     <li> <b> Automation & IOT Stuff </b> <SkillsFilter topic = "automation"/> </li>
                 </ol>
             </div>
