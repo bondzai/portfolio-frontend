d032378efb1145685b75efa629a57addd56ad114
JamesBond <introbond.dev@gmail.com>
2023-06-11 14:46:08 +0700

    Add feature: spin while loading

---
+++ frontend/src/components/SpinComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Spin } from "antd";
+import "../styles/SpinComponent.css";
+
+const SpinComponent = () => (
+  <div className="spin-container">
+    <Spin size="large" />
+  </div>
+);
+
+export default SpinComponent;
