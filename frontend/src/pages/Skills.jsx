cfcc52a665fcc02976838bb7499dd83bb93443ba
JamesBond <introbond.dev@gmail.com>
2023-06-11 10:04:02 +0700

    Update: skill filter bulk call

--- frontend/src/pages/Skills.jsx
+++ frontend/src/pages/Skills.jsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SkillsFilter from "../components/SkillsFilter";
+import { getSkillList } from "../apis/SkillList";
 import "../styles/Home.css";
 
 const Skills = () => {
+    const [skills, setSkills] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await getSkillList();
+            setSkills(result);
+        };
+        fetchData();
+    }, []);
+
     return (
         <div className="home">
             <div className="skills">  
                 <ol className="list">
-                    <li> <b> OS </b> <SkillsFilter topic = "os"/> </li>
-                    <li> <b> Programming Languages </b> <SkillsFilter topic = "language"/> </li>
-                    <li> <b> DevSecOps </b> <SkillsFilter topic = "tools"/> </li>
-                    <li> <b> Databases </b> <SkillsFilter topic = "database"/> </li>
-                    <li> <b> Backend </b> <SkillsFilter topic = "backend"/> </li>
-                    <li> <b> Frontend </b> <SkillsFilter topic = "frontend"/> </li>
-                    <li> <b> Automation & IOT Stuff </b> <SkillsFilter topic = "automation"/> </li>
+                    <li> <b> OS </b> <SkillsFilter topic="os" skills={skills}/> </li>
+                    <li> <b> Programming Languages </b> <SkillsFilter topic="language" skills={skills}/> </li>
+                    <li> <b> DevSecOps </b> <SkillsFilter topic="tools" skills={skills}/> </li>
+                    <li> <b> Databases </b> <SkillsFilter topic="database" skills={skills}/> </li>
+                    <li> <b> Backend </b> <SkillsFilter topic="backend" skills={skills}/> </li>
+                    <li> <b> Frontend </b> <SkillsFilter topic="frontend" skills={skills}/> </li>
+                    <li> <b> Automation & IOT Stuff </b> <SkillsFilter topic="automation" skills={skills}/> </li>
                 </ol>
             </div>
         </div>
