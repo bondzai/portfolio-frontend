1b66543f498804e8c7526da8e47a327ac657e699
JamesBond <introbond.dev@gmail.com>
2023-08-14 09:43:04 +0700

    Update: Calculate age functioni& exp

--- frontend/src/pages/Experience.jsx
+++ frontend/src/pages/Experience.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import IconSchool from "@material-ui/icons/School";
@@ -11,7 +11,7 @@ import "../styles/Experience.css";
 
 const Experience = () => {
     const [showScrollButton, setShowScrollButton] = useState(false);
-    
+
     useEffect(() => {
         const handleScroll = () => {
             if (window.pageYOffset > 100) {
@@ -31,11 +31,19 @@ const Experience = () => {
         window.scrollTo({ top: 0, behavior: "smooth" });
     };
 
-    const born = new Date("02/03/1994");
-    const ageMonth = Date.now() - born.getTime();
-    const ageDay = new Date(ageMonth);
-    const ageYear = ageDay.getUTCFullYear();
-    const age = Math.abs(ageYear - 1970);
+    const calculateAge = (birthdate) => {
+        const today = new Date();
+        const birthdateObj = new Date(birthdate);
+
+        const years = today.getFullYear() - birthdateObj.getFullYear();
+        const months = today.getMonth() - birthdateObj.getMonth();
+
+        if (months < 0 || (months === 0 && today.getDate() < birthdateObj.getDate())) {
+            return `${years - 1} year ${12 - birthdateObj.getMonth() + today.getMonth()} months`;
+        } else {
+            return `${years} years ${months} months`;
+        }
+    };
 
     return (
         <div className="experience">
@@ -47,7 +55,7 @@ const Experience = () => {
                     iconStyle={{ background: "#3e497a", color: "#fff" }}
                 >
                     <h3 className="vertical-timeline-element-title"> Born in Ubon Rachatani, Thailand </h3>
-                    <p> Age: {age} years old</p>
+                    <p> Age: {calculateAge("02/03/1994")}</p>
                 </VerticalTimelineElement>
 
                 <VerticalTimelineElement
@@ -79,8 +87,9 @@ const Experience = () => {
                     <h3 className="vertical-timeline-element-title"> Synergetech Co., Ltd., Nonthaburi, Thailand </h3>
                     <p> Role: Automation System Engineer </p>
                     <p> Responsibilities: Designed, developed, debugged, deployed & delivered industrial automation projects (especially automated batch control system). </p>
-                    <p> &emsp; - PLC, SCADA & Database programming. </p>
-                    <p> &emsp; - Implement analog signals, digital devices, I/O server & other 3rd party integrations by any protocol of choice. </p>
+                    <p> &emsp; - PLC, SCADA MES & Database programming. <br />
+                        &emsp; - Implement analog signals, digital devices, I/O server & other 3rd party integrations by any protocol of choices. <br />
+                    </p>
                 </VerticalTimelineElement>
 
                 <VerticalTimelineElement
@@ -90,28 +99,36 @@ const Experience = () => {
                     iconStyle={{ background: "#3e497a", color: "#fff" }}
                 >
                     <h3 className="vertical-timeline-element-title"> Self-learning: Software Development </h3>
-                    <p> Q2: </p>
-                    <p> &emsp; - Traditional system programming practice. Laser focus on logic, loop, data structure & algorithm. </p>
-                    <p> &emsp; - Built blockchain's data monitoring mobile application using Google cloud environment(GoogleSheet, AppSheet, AppsScript). </p>
-                    <p> Q3: </p>
-                    <p> &emsp; - Start using Git & GitHub. </p>
-                    <p> &emsp; - Start learning web development libraries & frameworks.(JavaScript stack - React.js & Express.js) </p>
-                    <p> Q4: </p>
-                    <p> &emsp; - Start learning Python-Django & software life cycle. </p>
-                    <p> &emsp; - Become a better backend developer. Pushing more open-source projects. Further down the road, keep learning. </p>
+                    <p> Q2: <br />
+                        &emsp; - Traditional system programming practice. Laser focus on logics, data structures & algorithms. <br />
+                        &emsp; - Built blockchain's data monitoring mobile application using Google cloud environment(GoogleSheet, AppSheet, AppsScript). <br />
+                    </p>
+                    <p> Q3: <br />
+                        &emsp; - Start using Git & GitHub. <br />
+                        &emsp; - Start learning web development libraries & frameworks.(JavaScript stack - React.js & Express.js). <br />
+                    </p>
+                    <p> Q4: <br />
+                        &emsp; - Learn more about backend services (Python-Django, Go-Fiber), infrasturcture tools & software life cycle. <br />
+                        &emsp; - Become a better backend developer. Pushing more open-source projects. Further down the road, keep learning. <br />
+                    </p>
                 </VerticalTimelineElement>
 
                 <VerticalTimelineElement
                     className="vertical-timeline-elemt--education"
-                    date="Nov 2022 - Present"
+                    date={`Nov 2022 - Present (${calculateAge("11/03/2022")})`}
                     icon={<IconWork />}
                     iconStyle={{ background: "royalblue", color: "#fff" }}
                 >
                     <h3 className="vertical-timeline-element-title"> Swift Dynamics Co., Ltd., Bangkok, Thailand </h3>
                     <p> Role: Backend Developer </p>
                     <p> Responsibilities: <br />
-                        &emsp; -Fully focus on design & develop many type of backend services (Database, Bussiness logic & API). <br />
-                        &emsp; -Co-work with peer engineers in coding & code reviews. <br />
+                        &emsp; - Fully focus on design & develop many type of backend services. <br />
+                        &emsp; - Co-work with peer engineers in coding & code reviews. <br />
+                    </p>
+                    <p> Challenges: <br />
+                        &emsp; - Become a master backend engineer. <br />
+                        &emsp; - Deep understanding about software architectural patterns (especially microservices event driven base architecture). <br />
+                        &emsp; - Learn more about DevSecOps & cloud engines. <br />
                     </p>
 
                 </VerticalTimelineElement>
