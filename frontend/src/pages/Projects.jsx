4b6a506a6cf1da4dba0edb8fc032537a0f39dca4
JamesBond <introbond.dev@gmail.com>
2023-08-14 08:48:21 +0700

    Update: Pagination size

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -93,11 +93,11 @@ const Projects = () => {
                             initialState={{
                                 pagination: {
                                     paginationModel: {
-                                        pageSize: 20,
+                                        pageSize: 10,
                                     },
                                 },
                             }}
-                            pageSizeOptions={[15, 20]}
+                            pageSizeOptions={[5, 10, 15]}
                             disableRowSelectionOnClick
                         />
                     </Box>
