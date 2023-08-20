dfccb4a83a71874637ee793ca169fc72a97316f4
JamesBond <introbond.dev@gmail.com>
2023-08-20 18:20:04 +0700

    Update: Github calendar style

--- frontend/src/components/Github.jsx
+++ frontend/src/components/Github.jsx
@@ -6,27 +6,31 @@ function Github() {
 
     return (
         <div className="github-calendar-background">
-        <div className="github-calendar-container">
-            <h4> Github Calendar </h4> <br />
-            <div className="github-calendar">
-                <GitHubCalendar
-                    username="introbond"
-                    blockSize={15}
-                    blockMargin={5}
-                    fontSize={16}
-                    theme={{
-                        level0: '#2d333b',
-                        level1: '#0e4429',
-                        level2: '#006d32',
-                        level3: '#26a641',
-                        level4: '#39d353'
-                    }}
-                    hideColorLegend
-                    showWeekdayLabels
-                />
+            <div className="github-calendar-container">
+
+                <div className='github-calendar-title-center'>
+                    <h3>Github Calendar</h3>
+                </div>
+
+                <div className="github-calendar">
+                    <GitHubCalendar
+                        username="introbond"
+                        blockSize={15}
+                        blockMargin={5}
+                        fontSize={16}
+                        theme={{
+                            level0: '#2d333b',
+                            level1: '#0e4429',
+                            level2: '#006d32',
+                            level3: '#26a641',
+                            level4: '#39d353'
+                        }}
+                        hideColorLegend
+                        showWeekdayLabels
+                    />
+                </div>
             </div>
         </div>
-    </div>
     );
 }
 
