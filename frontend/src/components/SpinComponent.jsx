85de224be888977d2a1f168ed5226ff7ccd34171
JamesBond <introbond.dev@gmail.com>
2023-08-20 17:27:52 +0700

    Update: Override custom spin dot style

--- frontend/src/components/SpinComponent.jsx
+++ frontend/src/components/SpinComponent.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Spin } from "antd";
 import "../styles/SpinComponent.css";
 
-const SpinComponent = () => (
-    <div className="spin-container">
-        <Spin size="large" />
-    </div>
-);
+const SpinComponent = ({ customColor }) => {
+    const dynamicStyle = {
+        "--custom-spin-color": customColor || "#1677ff",
+    };
+
+    return (
+        <div className="spin-container" style={dynamicStyle}>
+            <Spin size="large" />
+        </div>
+    );
+};
+
+SpinComponent.propTypes = {
+    customColor: PropTypes.string,
+};
 
 export default SpinComponent;
