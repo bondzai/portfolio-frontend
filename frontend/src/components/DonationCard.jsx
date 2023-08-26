32f9737d1933153e6a8c10a9fe21b4c0e6083077
JamesBond <introbond.dev@gmail.com>
2023-08-26 13:00:54 +0700

    Feature: Donation card

--- frontend/src/components/DonationCard.jsx
+++ frontend/src/components/DonationCard.jsx
@@ -1,22 +1,31 @@
-import React from 'react';
-import { Input, QRCode, Space } from 'antd';
+import React, { useState } from 'react';
+import { Input, QRCode, Space, Select } from 'antd';
+
+const options = [
+    {
+        value: '0x1c45e086ed143aef83c1209521a2ff5369f39abc',
+        label: 'ETH',
+    },
+    {
+        value: '0x2f35e086ed143aef83c1209521a2ff5369f39def',
+        label: 'BSC',
+    },
+];
 
 const DonationCard = () => {
-    const [text, setText] = React.useState('https://ant.design/');
-    
+    const [selectedValue, setSelectedValue] = useState(options[0].value);
+
     return (
         <Space direction="vertical" align="center">
-            <QRCode 
-                value={text || '-'}
-                // color= "#fff"
-                // bgColor=
-            />
-            <Input
-                placeholder="-"
-                maxLength={60}
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-            />
+            <QRCode value={selectedValue || '-'} />
+            <Space.Compact>
+                <Select
+                    defaultValue={selectedValue}
+                    options={options}
+                    onChange={value => setSelectedValue(value)}
+                />
+                <Input value={selectedValue} />
+            </Space.Compact>
         </Space>
     );
 };
