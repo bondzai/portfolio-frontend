76316e605eb42692090caa854db44e5da798aad8
JamesBond <introbond.dev@gmail.com>
2023-08-26 12:13:26 +0700

    Add: Donation card

---
+++ frontend/src/components/DonationCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Input, QRCode, Space } from 'antd';
+
+const DonationCard = () => {
+    const [text, setText] = React.useState('https://ant.design/');
+    
+    return (
+        <Space direction="vertical" align="center">
+            <QRCode 
+                value={text || '-'}
+                // color= "#fff"
+                // bgColor=
+            />
+            <Input
+                placeholder="-"
+                maxLength={60}
+                value={text}
+                onChange={(e) => setText(e.target.value)}
+            />
+        </Space>
+    );
+};
+
+export default DonationCard;
