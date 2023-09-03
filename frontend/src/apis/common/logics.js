9377341e55df9f690989e6f8493ba380c158814a
JamesBond <introbond.dev@gmail.com>
2023-09-03 12:31:29 +0700

    Refactor: Common connection logic

---
+++ frontend/src/apis/common/logics.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { CustomSortEnum } from '../../utils/choices.js';
+
+const customSortReponse = (customSort, response) => {
+    if (customSort == CustomSortEnum.DESCENDING) {
+        return response.data.sort((a, b) => b.id - a.id);
+    }
+
+    if (customSort == CustomSortEnum.ASCENDING) {
+        return response.data;
+    }
+
+    if (!customSort) {
+        return response.data;
+    }
+}
+
+const getList = (async ( {...Props} ) => {
+    let urls = Props.urls.map(url => url + Props.endpoint);
+
+    for (let url of urls) {
+        try {
+            const response = await axios.get(url);
+            return customSortReponse(Props.customSort, response)
+            // return response.data.sort((a, b) => b.id - a.id);
+        } catch (error) {
+            console.error(`Error with URL ${url}: ${error}`);
+        }
+    }
+    throw new Error('All backend services are unavailable.');
+});
+
+export { getList }
