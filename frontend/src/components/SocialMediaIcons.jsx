67f1f8a6c48d90734dec0b4e942a9677971428ad
JamesBond <introbond.dev@gmail.com>
2023-09-04 21:03:06 +0700

    Test: Bitcoin icon open model

--- frontend/src/components/SocialMediaIcons.jsx
+++ frontend/src/components/SocialMediaIcons.jsx
@@ -4,6 +4,9 @@ import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import MailIcon from '@material-ui/icons/Mail';
 import { SiGitbook } from 'react-icons/si';
+import { SiBitcoin } from 'react-icons/si';
+import DonationCard from "../components/DonationCard";
+import CustomModalButton from "../components/CustomModalButton";
 
 const SocialMediaIcons = () => {
     const openInNewTab = (url) => {
@@ -14,28 +17,38 @@ const SocialMediaIcons = () => {
         <div>
             <MailIcon
                 className="icon-social"
-                style={{ bottom: '250px', right: '40px' }}
+                style={{ bottom: '3000px', right: '40px' }}
                 onClick={() => openInNewTab('')}
             />
             <FacebookIcon
                 className="icon-social"
-                style={{ bottom: '200px', right: '40px' }}
+                style={{ bottom: '250px', right: '40px' }}
                 onClick={() => openInNewTab('')}
             />
             <LinkedInIcon
                 className="icon-social"
-                style={{ bottom: '150px', right: '40px' }}
+                style={{ bottom: '200px', right: '40px' }}
                 onClick={() => openInNewTab('')}
             />
             <SiGitbook
                 className="icon-social"
-                style={{ bottom: '100px', right: '40px', width: '25px', height: '25px'}}
+                style={{ bottom: '150px', right: '40px', width: '25px', height: '25px' }}
                 onClick={() => openInNewTab('')}
             />
             <GitHubIcon
                 className="icon-social"
-                style={{ bottom: '50px', right: '40px' }}
+                style={{ bottom: '100px', right: '40px' }}
                 onClick={() => openInNewTab('https://github.com/introbond')}
+            />
+            <CustomModalButton
+                buttonText="Buy me a coffee"
+                title="Buy me a coffee"
+                content={<DonationCard />}
+                icon={<SiBitcoin
+                    className="icon-social"
+                    style={{ bottom: '50px', right: '40px', width: '25px', height: '25px' }}
+                />}
+                hideButtons
             />
         </div>
     );
