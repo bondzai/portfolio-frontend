76891865b73dcc001f6d94e2cdf6b3c91b2bb6c6
JamesBond <introbond.dev@gmail.com>
2023-09-09 13:36:58 +0700

    Feature: Tooltip

--- frontend/src/components/Skill.jsx
+++ frontend/src/components/Skill.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import { openInNewTab } from "../utils/utils.js"
+import { openInNewTab } from "../utils/utils.js";
+import { Tooltip } from "antd";
 
 const SkillItem = ({ ...skill }) => {
-    
     return (
         <div className="skill">
             <div>
-                <button onClick={() => openInNewTab(skill.url)}>
-                    <img
-                        src={skill.image_url}
-                        alt={skill.name}
-                        style={{ width: "50px", height: "auto" }}
-                    />
-                </button>
+                <Tooltip title={skill.name}>
+                    <button onClick={() => openInNewTab(skill.url)}>
+                        <img
+                            src={skill.image_url}
+                            alt={skill.name}
+                            style={{ width: "50px", height: "auto" }}
+                        />
+                    </button>
+                </Tooltip>
             </div>
         </div>
     );
