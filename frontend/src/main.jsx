8c7002f54c1dad0dcbb8047e3d92336e917c4338
JamesBond <introbond.dev@gmail.com>
2023-09-12 20:41:25 +0700

    Update: Query data with Apollo client

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { ApolloProvider } from '@apollo/client';
+import client from './apis/graphql/apolloClient'
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+    <ApolloProvider client={client}>
         <App />
-    </React.StrictMode>,
-)
+    </ApolloProvider>,
+);
