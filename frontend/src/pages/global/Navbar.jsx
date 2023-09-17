72ac4d3c2127a914ffff96390591c39492e34861
JamesBond <introbond.dev@gmail.com>
2023-09-17 20:44:14 +0700

    Refactor

--- frontend/src/pages/global/Navbar.jsx
+++ frontend/src/pages/global/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-// import ReorderIcon from "@material-ui/icons/Reorder";
 import { GiHamburgerMenu } from 'react-icons/gi';
 
 import '../../styles/Navbar.css';
