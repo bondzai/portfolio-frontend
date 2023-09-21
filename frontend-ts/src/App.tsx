ce610dc4096b7993ec5836276b69d214ed8921aa
JamesBond <introbond.dev@gmail.com>
2023-09-21 18:58:37 +0700

    Learn from Ninja

--- frontend-ts/src/App.tsx
+++ frontend-ts/src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import React, { ReactElement } from 'react'; // Import React and ReactElement type
 
 import Navbar from './components/global/Navbar';
-import Home from './pages/Home';
-// import Skills from './pages/Skills';
-// import Projects from './pages/Projects';
-// import Certifications from './pages/Certifications';
-// import Experience from './pages/Experience';
-// import Stats from './pages/Stats';
-// import Roadmap from './pages/Roadmap';
+
+const Home = lazy(() => import('./pages/Home'));
+// const Skills = lazy(() => import('./pages/Skills'));
+// const Projects = lazy(() => import('./pages/Projects'));
+// const Certifications = lazy(() => import('./pages/Certifications'));
+// const Experience = lazy(() => import('./pages/Experience'));
+// const Stats = lazy(() => import('./pages/Stats'));
+// const Roadmap = lazy(() => import('./pages/Roadmap'));
 
 // import DisplayModal from './components/DisplayModal';
 
@@ -16,25 +19,24 @@ import Home from './pages/Home';
 
 import './App.css';
 
-function App(): JSX.Element {
-    return (
-        <div className="App">
-            <Router basename='/'>
-                <Navbar />
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    {/* <Route path='/skills' element={<Skills />} />
-                    <Route path='/projects' element={<Projects />} />
-                    <Route path='/project/:id' element={<DisplayModal getDataList={getProjectList} dataRoutePath="/projects" />} />
-                    <Route path='/certifications' element={<Certifications />} />
-                    <Route path='/certification/:id' element={<DisplayModal getDataList={getCertificationList} dataRoutePath="/certifications" />} />
-                    <Route path='/experience' element={<Experience />} />
-                    <Route path='/roadmap' element={<Roadmap />} />
-                    <Route path='/stats' element={<Stats />} /> */}
-                </Routes>
-            </Router>
-        </div>
-    );
-}
+const App: React.FC = (): ReactElement => ( // Add types to App component
+    <div className="App">
+        <Router basename='/'>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Suspense fallback={<div>Loading...</div>}><Home /></Suspense>} />
+                {/* <Route path='/skills' element={<Suspense fallback={<div>Loading...</div>}><Skills /></Suspense>} />
+                <Route path='/projects' element={<Suspense fallback={<div>Loading...</div>}><Projects /></Suspense>} />
+                <Route path='/project/:id' element={<DisplayModal getDataList={getProjectList} dataRoutePath="/projects" />} />
+                <Route path='/certifications' element={<Suspense fallback={<div>Loading...</div>}><Certifications /></Suspense>} />
+                <Route path='/certification/:id' element={<DisplayModal getDataList={getCertificationList} dataRoutePath="/certifications" />} />
+                <Route path='/experience' element={<Suspense fallback={<div>Loading...</div>}><Experience /></Suspense>} />
+                <Route path='/roadmap' element={<Suspense fallback={<div>Loading...</div>}><Roadmap /></Suspense>} />
+                <Route path='/stats' element={<Suspense fallback={<div>Loading...</div>}><Stats /></Suspense>} /> */}
+            </Routes>
+        </Router>
+    </div>
+);
 
 export default App;
+                
