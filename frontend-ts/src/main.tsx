013d754ff31110ee78c32b626ae782b9297f881a
JamesBond <introbond.dev@gmail.com>
2023-09-22 19:39:49 +0700

    Refactor

--- frontend-ts/src/main.tsx
+++ frontend-ts/src/main.tsx
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(
     <React.StrictMode>
         <BrowserRouter basename={import.meta.env.PROD ? '/portfolio/' : '/'}>
             <App />
