fcae362aacdbb1ccfaec646aed7aae2c5af3a1e8
JamesBond <introbond.dev@gmail.com>
2023-09-22 19:41:19 +0700

    Update: EXP page and withLoading HOC

--- frontend-ts/src/routes/index.tsx
+++ frontend-ts/src/routes/index.tsx
@@ -1,9 +1,11 @@
-import { lazy } from 'react'
-import { RouteObject } from 'react-router-dom'
-// import { Loader } from '../components/Loader'
+import { lazy } from "react"
+import { RouteObject } from "react-router-dom"
+import { withLoading } from "../components/hocs/withLoading"
 
-const Home = lazy(() => import('../pages/Home'))
+const Home = withLoading(lazy(() => import("../pages/Home")))
+const Experience = withLoading(lazy(() => import("../pages/Experience")))
 
 export const Routes: RouteObject[] = [
     { path: '/', element: <Home /> },
+    { path: '/experience', element: <Experience /> },
 ]
