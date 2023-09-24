45f801edef88569eabd3da2ea6448769e962379f
JamesBond <introbond.dev@gmail.com>
2023-09-24 09:39:20 +0700

    Feature: Dev link

--- frontend-ts/src/routes/index.tsx
+++ frontend-ts/src/routes/index.tsx
@@ -1,21 +1,32 @@
-import { lazy } from "react"
-import { RouteObject } from "react-router-dom"
-import { withLoading } from "../components/hocs/withLoading"
+import { lazy } from "react";
+import { RouteObject } from "react-router-dom";
+import { withLoading } from "../components/hocs/withLoading";
 
-const Home = withLoading(lazy(() => import("../pages/Home")))
-const Experience = withLoading(lazy(() => import("../pages/Experience")))
-const Skills = withLoading(lazy(() => import("../pages/Skills")))
-const Certifications = withLoading(lazy(() => import("../pages/Certifications")))
-const Stats = withLoading(lazy(() => import("../pages/Stats")))
-const Labs = withLoading(lazy(() => import("../pages/Labs")))
-const Roadmap = withLoading(lazy(() => import("../pages/Roadmap")))
+const Home = withLoading(lazy(() => import("../pages/Home")));
+const Experience = withLoading(lazy(() => import("../pages/Experience")));
+const Skills = withLoading(lazy(() => import("../pages/Skills")));
+const Certifications = withLoading(lazy(() => import("../pages/Certifications")));
+const Stats = withLoading(lazy(() => import("../pages/Stats")));
+const Labs = withLoading(lazy(() => import("../pages/Labs")));
+const Roadmap = withLoading(lazy(() => import("../pages/Roadmap")));
 
 export const Routes: RouteObject[] = [
-    { path: '/', element: <Home /> },
-    { path: '/experience', element: <Experience /> },
-    { path: '/skills', element: <Skills /> },
+    { path: "/", element: <Home /> },
+    { path: "/experience", element: <Experience /> },
+    { path: "/skills", element: <Skills /> },
     { path: "/labs", element: <Labs /> },
-    { path: '/certifications', element: <Certifications /> },
+    { path: "/certifications", element: <Certifications /> },
     { path: "/stats", element: <Stats /> },
     { path: "/roadmap", element: <Roadmap /> },
-]
+];
+
+if (import.meta.env.DEV) {
+    const DevelopmentPage = withLoading(lazy(() => import("../pages/Development")))
+    
+    Routes.push(
+        {
+            path: "/development",
+            element: <DevelopmentPage />,
+        },
+    )
+}
