af433b66fd140bd6d98e4b00efba438669d6a228
JamesBond <introbond.dev@gmail.com>
2023-09-24 19:25:30 +0700

    Remove test component

--- frontend-ts/src/pages/Development.tsx
+++ frontend-ts/src/pages/Development.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import TestTree from "../components/testTree"
 
 const Development: React.FC = () => {    
     return (
         <div>
             hello
-            <TestTree />
         </div>
     )
 }
