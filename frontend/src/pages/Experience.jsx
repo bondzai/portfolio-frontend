c4f3048693934c3521a2b8534c46a3d9bd5682c9
JamesBond <introbond.dev@gmail.com>
2023-12-29 16:30:00 +0700

    Refactor

--- frontend/src/pages/Experience.jsx
+++ frontend/src/pages/Experience.jsx
@@ -1,17 +1,46 @@
 import React from "react";
-
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
-
 import { IoMdPulse, IoMdSchool } from "react-icons/io";
 import { FaBookOpenReader } from "react-icons/fa6";
 import { MdWork } from "react-icons/md";
-
 import ScrollButton from "../components/ScrollButon";
-
 import useScroll from "../hooks/useScroll";
-
 import "react-vertical-timeline-component/style.min.css";
 import "../styles/Experience.css";
+
+const TimelineElement = ({ date, icon, iconStyle, title, content }) => {
+    const formatKeyValue = (text) => {
+        const parts = text.split(':');
+        if (parts.length === 2) {
+            return (
+                <>
+                    <strong>{parts[0]}</strong>:{parts[1]}
+                </>
+            );
+        } else {
+            return text;
+        }
+    };
+
+    const formattedContent = content.split('\n').map((item, index) => (
+        <React.Fragment key={index}>
+            {formatKeyValue(item)}
+            <br />
+        </React.Fragment>
+    ));
+
+    return (
+        <VerticalTimelineElement
+            className="vertical-timeline-elemt--education"
+            date={date}
+            icon={icon}
+            iconStyle={iconStyle}
+        >
+            <h3 className="vertical-timeline-element-title">{title}</h3>
+            <p>{formattedContent}</p>
+        </VerticalTimelineElement>
+    );
+};
 
 
 const Experience = () => {
@@ -33,90 +62,75 @@ const Experience = () => {
     return (
         <div className="experience">
             <VerticalTimeline lineColor="#3e497a">
-                <VerticalTimelineElement
-                    className="vertical-timeline-elemt--education"
-                    date="Feb 1994"
+                <TimelineElement
+                    date={`Feb 1994 (Age: ${calculateAge("02/03/1994")})`}
                     icon={<IoMdPulse />}
                     iconStyle={{ background: "#3e497a", color: "#fff" }}
-                >
-                    <h3 className="vertical-timeline-element-title"> Born in Ubon Rachatani, Thailand </h3>
-                    <p> Age: {calculateAge("02/03/1994")}</p>
-                </VerticalTimelineElement>
+                    title="Born in Ubon Rachatani, Thailand"
+                    content=""
+                />
 
-                <VerticalTimelineElement
-                    className="vertical-timeline-elemt--education"
+                <TimelineElement
                     date="2009 - 2012 (3 years)"
                     icon={<IoMdSchool />}
                     iconStyle={{ background: "#3e497a", color: "#fff" }}
-                >
-                    <h3 className="vertical-timeline-element-title"> Benchama Maharat Ubon Ratchatani </h3>
-                    <p> High School Diploma: Sci-Math</p>
-                </VerticalTimelineElement>
+                    title="Benchama Maharat Ubon Ratchatani"
+                    content="High School Diploma Sci-Math"
+                />
 
-                <VerticalTimelineElement
-                    className="vertical-timeline-elemt--education"
+                <TimelineElement
                     date="2012 - 2016 (4 years)"
                     icon={<IoMdSchool />}
                     iconStyle={{ background: "#3e497a", color: "#fff" }}
-                >
-                    <h3 className="vertical-timeline-element-title"> King Mongkut's Institute of Technology Ladkrabang </h3>
-                    <p> Bachelor's Degree: Automation Engineering </p>
-                </VerticalTimelineElement>
+                    title="King Mongkut's Institute of Technology Ladkrabang"
+                    content="Bachelor's Degree Automation Engineering"
+                />
 
-                <VerticalTimelineElement
-                    className="vertical-timeline-elemt--education"
+                <TimelineElement
                     date="Jul 2016 - Jan 2022 (5 years 7 months)"
                     icon={<MdWork />}
                     iconStyle={{ background: "royalblue", color: "#fff" }}
-                >
-                    <h3 className="vertical-timeline-element-title"> Synergetech Co., Ltd., Nonthaburi, Thailand </h3>
-                    <p> Role: Automation System Engineer </p>
-                    <p> Responsibilities: <br />
-                        &emsp; - Designed, developed, debugged, deployed & delivered industrial automation projects (especially automated batch control system). <br />
-                        &emsp; - PLC, SCADA MES & Database programming. <br />
-                        &emsp; - Implement analog signals, digital devices, I/O server & other 3rd party integrations by any protocol of choices. <br />
-                    </p>
-                </VerticalTimelineElement>
+                    title="Synergetech Co., Ltd., Nonthaburi, Thailand"
+                    content={`Role: Automation System Engineer
+                    Responsibilities:
+                        - Designed, developed, debugged, deployed & delivered industrial automation projects (especially automated batch control system).
+                        - Industrial automation & control system programming. (PLC, SCADA, MES & Database)
+                        - Implement analog signals, digital devices, I/O server & other 3rd party integrations by any protocol of choices.
+                    `}
+                />
 
-                <VerticalTimelineElement
-                    className="vertical-timeline-elemt--education"
+                <TimelineElement
                     date="Jan 2022 - Nov 2022 (10 months)"
                     icon={<FaBookOpenReader />}
                     iconStyle={{ background: "#3e497a", color: "#fff" }}
-                >
-                    <h3 className="vertical-timeline-element-title"> Self-learning: Software Development </h3>
-                    <p> Q2: <br />
-                        &emsp; - Traditional system programming practice. Laser focus on logics, data structures & algorithms. <br />
-                        &emsp; - Built blockchain's data monitoring mobile application using Google cloud environment(GoogleSheet, AppSheet, AppsScript). <br />
-                    </p>
-                    <p> Q3: <br />
-                        &emsp; - Start using Git & GitHub. <br />
-                        &emsp; - Start learning web development libraries & frameworks.(JavaScript stack - React.js & Express.js). <br />
-                    </p>
-                    <p> Q4: <br />
-                        &emsp; - Learn more about backend services (Python-Django, Go-Fiber), infrasturcture tools & software life cycle. <br />
-                        &emsp; - Become a better backend developer. Pushing more open-source projects. Further down the road, keep learning. <br />
-                    </p>
-                </VerticalTimelineElement>
+                    title="Self-learning: Software Development"
+                    content={`Q2:
+                        - Traditional system programming practice. Laser focus on logics, data structures & algorithms.
+                        - Built blockchain's data monitoring mobile application using Google cloud environment(GoogleSheet, AppSheet, AppsScript).
+                    Q3:
+                        - Start using Git & GitHub.
+                        - Start learning web development libraries & frameworks.(JavaScript stack - React.js & Express.js).
+                    Q4:
+                        - Learn more about backend services (Python-Django, Go-Fiber), infrasturcture tools & software life cycle.
+                    `}
+                />
 
-                <VerticalTimelineElement
-                    className="vertical-timeline-elemt--education"
+                <TimelineElement
                     date={`Nov 2022 - Present (${calculateAge("11/03/2022")})`}
                     icon={<MdWork />}
                     iconStyle={{ background: "royalblue", color: "#fff" }}
-                >
-                    <h3 className="vertical-timeline-element-title"> Swift Dynamics Co., Ltd., Bangkok, Thailand </h3>
-                    <p> Role: Backend Developer </p>
-                    <p> Responsibilities: <br />
-                        &emsp; - Fully focus on design & develop many type of backend services. <br />
-                        &emsp; - Co-work with peer engineers in coding & code reviews. <br />
-                    </p>
-                    <p> Challenges: <br />
-                        &emsp; - Become a master backend engineer. <br />
-                        &emsp; - Deep understanding about software architectural patterns (especially microservices event driven base architecture). <br />
-                        &emsp; - Learn more about DevSecOps & cloud engines. <br />
-                    </p>
-                </VerticalTimelineElement>
+                    title="Swift Dynamics Co., Ltd., Bangkok, Thailand"
+                    content={`Role: Backend Developer
+                    Responsibilities:
+                        - Fully focus on design & develop many types of backend services.
+                        - Co-work with peer engineers in coding & code reviews.
+                    Challenges:
+                        - Become a master backend engineer.
+                        - Deep understanding of software architectural patterns (especially microservices event-driven base architecture).
+                        - Learn more about DevSecOps & cloud engines.
+                    `}
+                />
+
             </VerticalTimeline>
             <ScrollButton
                 showScrollButton={showScrollButton}
@@ -124,7 +138,7 @@ const Experience = () => {
                 scrollToBottom={scrollToBottom}
             />
         </div>
-    )
-}
+    );
+};
 
 export default Experience;
