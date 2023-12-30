cfaad437028d833d3347cac2a3e3003ffae4a1e1
JamesBond <introbond.dev@gmail.com>
2023-12-30 09:25:53 +0700

    Refactor GetDataFromAPI function to use a custom type for GoogleSheetData

--- backend/services/google-sheet/sheet_queires.go
+++ backend/services/google-sheet/sheet_queires.go
@@ -5,7 +5,9 @@ import (
 	"net/http"
 )
 
-func GetDataFromAPI(dataType string) ([]map[string]interface{}, error) {
+type GoogleSheetData []map[string]interface{}
+
+func GetDataFromAPI(dataType string) (GoogleSheetData, error) {
 	url := getGoogleSheetURL() + dataType
 
 	resp, err := http.Get(url)
@@ -14,31 +16,26 @@ func GetDataFromAPI(dataType string) ([]map[string]interface{}, error) {
 	}
 	defer resp.Body.Close()
 
-	var data []map[string]interface{}
+	var data GoogleSheetData
 	err = json.NewDecoder(resp.Body).Decode(&data)
 	if err != nil {
 		return nil, err
 	}
 
-	filterData(&data, "is_showing", false)
+	data.filter("is_showing", false)
 
 	return data, nil
 }
 
-func filterData(data *[]map[string]interface{}, key string, value interface{}) {
-	for i, item := range *data {
-		if item[key] == value {
-			removeElement(data, i)
+func (data *GoogleSheetData) filter(key string, value interface{}) {
+	dstIndex := 0
+
+	for _, item := range *data {
+		if item[key] != value {
+			(*data)[dstIndex] = item
+			dstIndex++
 		}
 	}
-}
 
-func removeElement(slice *[]map[string]interface{}, index int) {
-	if index < 0 || index >= len(*slice) {
-		return
-	}
-
-	copy((*slice)[index:], (*slice)[index+1:])
-
-	*slice = (*slice)[:len(*slice)-1]
+	*data = (*data)[:dstIndex]
 }
