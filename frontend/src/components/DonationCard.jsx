0b4507e1014a90bd240b63d589390b639e6fa31e
JamesBond <introbond.dev@gmail.com>
2024-03-31 16:55:17 +0700

    Update cryptocurrency options in DonationCard component

--- frontend/src/components/DonationCard.jsx
+++ frontend/src/components/DonationCard.jsx
@@ -6,14 +6,20 @@ const options = [
     {
         id: 1,
         value: "0x1c45e086ed143aef83c1209521a2ff5369f39abc",
-        label: "ETH",
-        iconUrl: "https://www.vectorlogo.zone/logos/ethereum/ethereum-icon.svg",
+        label: "BTC",
+        iconUrl: "https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=029",
     },
     {
         id: 2,
         value: "0x1c45e086ed143aef83c1209521a2ff5369f39abc",
-        label: "BTC",
-        iconUrl: "https://www.vectorlogo.zone/logos/bitcoin/bitcoin-icon.svg",
+        label: "ETH",
+        iconUrl: "https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=029",
+    },
+    {
+        id: 3,
+        value: "0x1c45e086ed143aef83c1209521a2ff5369f39abc",
+        label: "AVAX",
+        iconUrl: "https://cryptologos.cc/logos/avalanche-avax-logo.svg?v=029",
     },
 ];
 
