ec413a56478471af90d2ac630a57116911335b6c
JamesBond <introbond.dev@gmail.com>
2024-04-13 09:54:54 +0700

    Update cryptocurrency labels in Wallets.js

--- frontend/src/apis/rest/Wallets.js
+++ frontend/src/apis/rest/Wallets.js
@@ -2,19 +2,19 @@ export const cryptoWallets = [
     {
         id: 1,
         value: "0x1c45e086ed143aef83c1209521a2ff5369f39abc",
-        label: "BTC",
+        label: "WBTC - Ethereum Network",
         iconUrl: "https://cryptologos.cc/logos/bitcoin-btc-logo.svg?v=029",
     },
     {
         id: 2,
         value: "0x1c45e086ed143aef83c1209521a2ff5369f39abc",
-        label: "ETH",
+        label: "ETH - Ethereum Network",
         iconUrl: "https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=029",
     },
     {
         id: 3,
         value: "0x1c45e086ed143aef83c1209521a2ff5369f39abc",
-        label: "AVAX",
+        label: "AVAX - Avalanche C-Chain Network",
         iconUrl: "https://cryptologos.cc/logos/avalanche-avax-logo.svg?v=029",
     },
 ];
