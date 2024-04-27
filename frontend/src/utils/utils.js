c516dbb5024443b3c2095c958d3a2889261b66b7
JamesBond <introbond.dev@gmail.com>
2024-04-27 19:59:35 +0700

    Feat: mail to

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,3 +1,7 @@
 export const openInNewTab = (url) => {
     window.open(url, "_blank", "noopener,noreferrer");
 };
+
+export const mailTo = (email) => {
+    window.location.href = `mailto:${email}`;
+};
