f63308b056212b5a9710a63eb28419387d1113a3
JamesBond <introbond.dev@gmail.com>
2024-05-01 18:54:55 +0700

    Feat: Conditional rendering

--- frontend/src/pages/global/Sidebar.jsx
+++ frontend/src/pages/global/Sidebar.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { Space } from "antd";
 import SocialMediaIcons from "../../components/SocialMediaIcons";
 import "./Sidebar.css";
 
 const Sidebar = () => {
+    const location = useLocation();
+
+    console.log(location)
+
+    const excludedPaths = ["/experience"];
+
+    const shouldRenderSidebar = () => {
+        return !excludedPaths.includes(location.pathname);
+    };
+
     return (
         <div className="">
-            <Space direction="vertical" size="large" style={{ display: "flex" }}>
-                <SocialMediaIcons />
-            </Space>
+            {shouldRenderSidebar() && (
+                <Space direction="vertical" size="large" style={{ display: "flex" }}>
+                    <SocialMediaIcons />
+                </Space>
+            )}
         </div>
     );
 };
