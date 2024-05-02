63881dbef37c206acc30d87364256ab4e879c38b
JamesBond <introbond.dev@gmail.com>
2024-05-02 19:59:47 +0700

    Feat: Responsive watchers

--- frontend/src/components/badges/Watchers.jsx
+++ frontend/src/components/badges/Watchers.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 import { EyeOutlined } from '@ant-design/icons';
 import { Avatar, Badge, Tooltip } from 'antd';
+import { isMobile } from "../../utils/constants.js";
+
 
 const Watchers = ({ activeUsersCount }) => {
+    const style = {
+        position: 'fixed',
+        padding: '5px 5px',
+        backgroundColor: 'var(--color-secondary)',
+        color: 'white',
+        border: 'none',
+        borderRadius: '50%',
+        cursor: 'pointer',
+        opacity: 0.95,
+        top: "15px",
+        right: "20px",
+    }
+
+    if (isMobile) {
+        style.right = "80px"
+        style.top = "9px"
+    }
+
     return (
-        <div
-            style={{
-                position: 'fixed',
-                padding: '10px 10px',
-                backgroundColor: 'var(--color-secondary)',
-                color: 'white',
-                border: 'none',
-                borderRadius: '50%',
-                cursor: 'pointer',
-                opacity: 0.95,
-                top: "8px",
-                right: "20px",
-            }}
-        >
-            <Tooltip title={`Active Users: ${activeUsersCount}`}>
-                <Badge count={activeUsersCount} style={{ backgroundColor: "green" }}>
+        <div style={style}>
+            <Tooltip title={`Watching: ${activeUsersCount}`}>
+                <Badge count={activeUsersCount} style={{ backgroundColor: "var(--color-secondary)" }}>
                     <Avatar shape="circle" icon={<EyeOutlined />} />
                 </Badge>
             </Tooltip>
