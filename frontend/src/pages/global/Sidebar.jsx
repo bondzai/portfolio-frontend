bc5e562ff74fe28c02f656ddf28e45aa033b0458
JamesBond <introbond.dev@gmail.com>
2024-05-03 23:23:15 +0700

    Refactor

--- frontend/src/pages/global/Sidebar.jsx
+++ frontend/src/pages/global/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useLocation } from "react-router-dom";
 import { Space} from 'antd';
 import SocialMediaIcons from "../../components/SocialMediaIcons";
