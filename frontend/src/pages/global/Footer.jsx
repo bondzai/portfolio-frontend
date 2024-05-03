c836e1b5f9eb408b9e5ff8d493653de548c71d2a
JamesBond <introbond.dev@gmail.com>
2024-05-04 06:21:36 +0700

    Refactor: Clean code

--- frontend/src/pages/global/Footer.jsx
+++ frontend/src/pages/global/Footer.jsx
@@ -1,21 +1,11 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 import Counter from "../../components/Counter";
 import "./Footer.css"
 
 const Footer = () => {
-    const location = useLocation();
-    const paths = ["/experience", "/skills", "/projects", "/certifications"];
-
-    const isRender = () => {
-        return !paths.includes(location.pathname);
-    };
-
     return (
         <div className="footer">
             <Counter />
-            {/* {isRender() && ( */}
-            {/* )} */}
         </div>
     );
 };
