d115381af46972210566fee53460087ab8167336
JamesBond <introbond.dev@gmail.com>
2024-05-03 23:26:53 +0700

    Refactor: Centralize footer

---
+++ frontend/src/pages/global/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+
+
+const Footer = () => {
+    const location = useLocation();
+    const excludedPaths = ["/experience", "/skills", "/projects", "/certifications"];
+
+    const isRender = () => {
+        return !excludedPaths.includes(location.pathname);
+    };
+
+    return (
+        <div className="footer">
+            {isRender() && (
+                <div>
+                    <p>
+                        <Counter />
+                    </p>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Footer;
