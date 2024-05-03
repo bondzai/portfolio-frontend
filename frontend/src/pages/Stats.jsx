076b37db180e62ac1082d25d1d535fa53a5eea3f
JamesBond <introbond.dev@gmail.com>
2024-05-03 23:46:22 +0700

    Refactor: Centralize footer

--- frontend/src/pages/Stats.jsx
+++ frontend/src/pages/Stats.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Space } from "antd";
 import WakatimeStat from "../components/WakatimeStats";
 import GitHub from "../components/GitHub";
-import Counter from "../components/Counter";
 import "../styles/Stats.css";
 
 const Stats = () => {
@@ -16,11 +15,6 @@ const Stats = () => {
                     <GitHub />
                 </div>
             </Space>
-            <div className="footer">
-                <div>
-                    <Counter />
-                </div>
-            </div>
         </div>
     );
 };
