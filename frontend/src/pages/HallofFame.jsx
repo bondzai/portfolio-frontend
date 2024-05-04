4efcce64b42834a84e9f014d67c744c64dedc305
JamesBond <introbond.dev@gmail.com>
2024-05-04 19:11:02 +0700

    WIP: Dynamic cube style

--- frontend/src/pages/HallofFame.jsx
+++ frontend/src/pages/HallofFame.jsx
@@ -3,10 +3,17 @@ import Cube from "../components/decorators/Cube.jsx"
 import "../styles/HallofFame.css";
 
 const HallofFame = () => {
+    const cubeStyle = {
+        "size": "200px",
+        "aura": "100px",
+        "transform": "100px",
+        "perspective": "800px",
+    }
+
     return (
         <div className="hof-background">
             <div>
-                <Cube />
+                <Cube {...cubeStyle}/>
             </div>
         </div>
     );
