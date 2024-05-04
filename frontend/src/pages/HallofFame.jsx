be816012a04c09d3fdd666e81a91a31b069d5297
JamesBond <introbond.dev@gmail.com>
2024-05-04 20:42:01 +0700

    Feat: Auto infinite scroll

--- frontend/src/pages/HallofFame.jsx
+++ frontend/src/pages/HallofFame.jsx
@@ -1,20 +1,74 @@
 import React from "react";
-import Cube from "../components/decorators/Cube.jsx"
 import "../styles/HallofFame.css";
+import { Avatar, Card } from 'antd';
+
+const { Meta } = Card;
+
+const cardDetails = {
+    1: {
+        avatar: "https://api.dicebear.com/7.x/miniavs/svg?seed=1",
+        title: "Text 1",
+        description: "This is the description 1"
+    },
+    2: {
+        avatar: "https://api.dicebear.com/7.x/miniavs/svg?seed=1",
+        title: "Text 2",
+        description: "This is the description 2"
+    },
+    3: {
+        avatar: "https://api.dicebear.com/7.x/miniavs/svg?seed=1",
+        title: "Text 3",
+        description: "This is the description 3"
+    },
+}
+
+const HeroCard = ({avatar, title, description}) => {
+    return (
+        <Card style={{ width: 300 }}>
+            <Meta
+                avatar={<Avatar src={avatar} />}
+                title={title}
+                description={description}
+            />
+        </Card>
+    )
+}
+
+function CarouselItem({props}) {
+    console.log("debug")
+    console.log(props)
+    return (
+        <HeroCard {...props}/>
+    );
+}
+
+function AutoplayCarousel() {
+    return (
+        <div className="carousel-container">
+            <div className="carousel-track">
+                {Object.keys(cardDetails).map((detailKey) => {
+                    return (
+                        <CarouselItem
+                            props={cardDetails[detailKey]}
+                        ></CarouselItem>
+                    );
+                })}
+                {Object.keys(cardDetails).map((detailKey) => {
+                    return (
+                        <CarouselItem
+                            props={cardDetails[detailKey]}
+                        ></CarouselItem>
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
 
 const HallofFame = () => {
-    const cubeStyle = {
-        "size": "200px",
-        "aura": "100px",
-        "transform": "100px",
-        "perspective": "800px",
-    }
-
     return (
         <div className="hof-background">
-            <div>
-                <Cube {...cubeStyle}/>
-            </div>
+            <AutoplayCarousel />
         </div>
     );
 };
