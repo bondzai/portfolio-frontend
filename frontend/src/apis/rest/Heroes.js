04aabe5972e8d93c28286457a469ab145878dfde
JamesBond <introbond.dev@gmail.com>
2024-05-05 10:20:50 +0700

    WIP: Update date

--- frontend/src/apis/rest/Heroes.js
+++ frontend/src/apis/rest/Heroes.js
@@ -2,21 +2,29 @@ export const cardDetails = {
     1: {
         avatar: "https://cdn-icons-png.flaticon.com/128/10441/10441773.png",
         title: "Ninja",
-        description: "https://chaninlaw.vercel.app/"
+        repoUrl: "https://chaninlaw.vercel.app/",
+        url: "https://chaninlaw.vercel.app/",
+        description: "",
     },
     2: {
         avatar: "https://cdn-icons-png.flaticon.com/128/520/520736.png",
         title: "Tum",
-        description: "https://nuttapat-portfolio.onrender.com/"
+        url: "https://nuttapat-portfolio.onrender.com/",
+        repoUrl: "https://github.com/nuttapat-swd",
+        description: "",
     },
     3: {
         avatar: "https://cdn-icons-png.flaticon.com/128/10376/10376391.png",
         title: "Fluke",
-        description: "https://jeerawut.vercel.app/"
+        url: "https://jeerawut.vercel.app/",
+        repoUrl: "https://github.com/jeerawut97/",
+        description: "",
     },
     4: {
         avatar: "https://cdn-icons-png.flaticon.com/128/5595/5595500.png",
         title: "Gug",
-        description: "https://bt144p-portfolio.vercel.app/"
+        url: "https://bt144p-portfolio.vercel.app/",
+        repoUrl: "https://github.com/B144p/",
+        description: "",
     },
 }
