4ffd8c1302bb2fdb4ee3ccb9b659ff27ce2d41fb
JamesBond <introbond.dev@gmail.com>
2024-05-05 10:21:07 +0700

    WIP: Update card style

--- frontend/src/components/cards/HeroCard.jsx
+++ frontend/src/components/cards/HeroCard.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { Avatar, Card } from 'antd';
+import { GithubFilled, LinkOutlined } from '@ant-design/icons';
 
 const { Meta } = Card;
 
 export const HeroCard = ({avatar, title, description}) => {
     return (
-        <Card style={{ width: "400px", height: "200px" }}>
+        <Card 
+            style={{ 
+                width: "360px",
+                height: "180px",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between"
+            }}
+            actions={[
+                <GithubFilled key="github" />,
+                <LinkOutlined key="link" />
+            ]}
+        >
             <Meta
                 avatar={<Avatar src={avatar} />}
                 title={title}
