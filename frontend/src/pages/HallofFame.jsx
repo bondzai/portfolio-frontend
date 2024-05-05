64e8bcfeac33f83a6cc632c177ad9e2b736b415c
JamesBond <introbond.dev@gmail.com>
2024-05-05 17:07:24 +0700

    WIP: Paragraph

--- frontend/src/pages/HallofFame.jsx
+++ frontend/src/pages/HallofFame.jsx
@@ -31,7 +31,18 @@ const AutoplayCarousel = () => {
 const HallofFame = () => {
     return (
         <div className="hof-background" >
-            <h3> Brotherhood </h3>
+            <div style={{width: "70%", marginBottom: "100px"}}>
+                <p>
+                    Step into the heart of my blog: the <strong>Brotherhood</strong>.
+                </p>
+                <p>
+                    Here, I pay homage to the remarkable souls who've guided and supported my software engineering journey. <br />
+                    Each name etched in this <strong>Hall of Fame</strong> reflects the profound impact of their mentorship. With deep respect and humility, <br />
+                    I invite you to explore the <strong>Brotherhood</strong>—a place where gratitude and resilience shine bright, illuminating our path forward.
+                </p>
+                Thanks for reading, <br />
+                James Bond
+            </div>
             <AutoplayCarousel />
             <br />
             <small> Last updated: 2024-05-05 </small>
