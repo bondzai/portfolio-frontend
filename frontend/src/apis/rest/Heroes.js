fd31bb840c1eb8d44a98008d6a2e3fe39a7c0510
JamesBond <introbond.dev@gmail.com>
2024-05-05 09:51:17 +0700

    Feat: Anonymous icons

--- frontend/src/apis/rest/Heroes.js
+++ frontend/src/apis/rest/Heroes.js
@@ -1,21 +1,21 @@
 export const cardDetails = {
     1: {
-        avatar: "https://api.dicebear.com/7.x/miniavs/svg?seed=1",
+        avatar: "https://cdn-icons-png.flaticon.com/128/10441/10441773.png",
         title: "Ninja",
         description: "https://chaninlaw.vercel.app/"
     },
     2: {
-        avatar: "https://api.dicebear.com/7.x/miniavs/svg?seed=1",
+        avatar: "https://cdn-icons-png.flaticon.com/128/520/520736.png",
         title: "Tum",
         description: "https://nuttapat-portfolio.onrender.com/"
     },
     3: {
-        avatar: "https://api.dicebear.com/7.x/miniavs/svg?seed=1",
+        avatar: "https://cdn-icons-png.flaticon.com/128/10376/10376391.png",
         title: "Fluke",
         description: "https://jeerawut.vercel.app/"
     },
     4: {
-        avatar: "https://api.dicebear.com/7.x/miniavs/svg?seed=1",
+        avatar: "https://cdn-icons-png.flaticon.com/128/11191/11191033.png",
         title: "Gug",
         description: "https://bt144p-portfolio.vercel.app/"
     },
