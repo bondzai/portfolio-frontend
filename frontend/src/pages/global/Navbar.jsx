04e50fd409d8acb5cd5d8f949d6021616a3e9eaa
JamesBond <introbond.dev@gmail.com>
2024-05-06 07:01:23 +0700

    Refactor: Decoupling websocket logic

--- frontend/src/pages/global/Navbar.jsx
+++ frontend/src/pages/global/Navbar.jsx
@@ -1,17 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
-import Watchers from "../../components/badges/Watchers";
 import "./Navbar.css";
 
-import { Statistic } from 'antd';
-import CountUp from 'react-countup';
-
-
-const formatter = (value) => <CountUp end={value} separator="," />;
-const TotalUsers = ({totalUsersCount}) => (
-    <Statistic title="Active Users" value={totalUsersCount} formatter={formatter} />
-);
 
 const Navbar = () => {
     const [expandNavbar, setExpandNavbar] = useState(false);
@@ -30,53 +21,6 @@ const Navbar = () => {
     useEffect(() => {
         setActiveLink(location.pathname.slice(1));
     }, [location]);
-
-    const [activeUsersCount, setActiveUsersCount] = useState(0);
-    const [totalUsersCount, setTotalUsersCount] = useState(0);
-    const wsUrl = import.meta.env.VITE_WS_URL;
-    let ws;
-
-    useEffect(() => {
-        ws = new WebSocket(wsUrl);
-
-        ws.onopen = () => {
-            console.log("WebSocket connected");
-        };
-
-        ws.onmessage = (event) => {
-            try {
-                const data = JSON.parse(event.data);
-                let activeUsers = parseInt(data.activeUsers);
-                if (!activeUsers || isNaN(activeUsers)) {
-                    activeUsers = 0;
-                }
-                setActiveUsersCount(activeUsers);
-
-                let totalUsers = parseInt(data.totalUsers);
-                if (!totalUsers || isNaN(totalUsers)) {
-                    totalUsers = 0;
-                }
-                setTotalUsersCount(totalUsers);
-            } catch (error) {
-                console.error('Error parsing JSON:', error);
-            }
-        };
-
-        ws.onclose = () => {
-            console.log("WebSocket disconnected");
-        };
-
-        window.addEventListener("beforeunload", handleBeforeUnload);
-
-        return () => {
-            window.removeEventListener("beforeunload", handleBeforeUnload);
-            ws.close();
-        };
-    }, []);
-
-    const handleBeforeUnload = () => {
-        ws.close();
-    };
 
     return (
         <div className="navbar">
@@ -109,7 +53,6 @@ const Navbar = () => {
                     Hall of Fame
                 </Link>
             </div>
-            <Watchers activeUsersCount={activeUsersCount} />
         </div>
     );
 };
