3a0f117508bc3968b1f3889191692105f5406f8e
JamesBond <introbond.dev@gmail.com>
2024-05-07 06:21:28 +0700

    Feat: Status bar

--- frontend/src/pages/global/Footer.jsx
+++ frontend/src/pages/global/Footer.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Counter from "../../components/Counter";
+import StatusIcons from "../../components/icons/StatusIcons";
 import "./Footer.css"
 
 const Footer = () => {
     return (
         <div className="footer">
+            <div className="status-icons">
+                <StatusIcons />
+            </div>
             <Counter />
         </div>
     );
