c01aae8ab377a35bcc02673b1d691eb53551f584
JamesBond <introbond.dev@gmail.com>
2024-05-08 21:24:41 +0700

    Feat: Status bar

--- frontend/src/components/icons/StatusIcons.jsx
+++ frontend/src/components/icons/StatusIcons.jsx
@@ -5,21 +5,23 @@ import {
   CodeOutlined,
   CoffeeOutlined,
   ReadOutlined,
-  SyncOutlined,
 } from '@ant-design/icons';
-import { Badge, Space } from 'antd';
+import { Badge, Space, Tooltip, Popover } from 'antd';
 
 
 const StatusIcons = () => (
-    <Space size={10}>
+    <Space>
         <Badge dot>
             <BellOutlined className="status-icon" />
         </Badge>
-        <DatabaseOutlined className="status-icon" />
-        <CodeOutlined className="status-icon" />
+        <Popover content="content" title="Title" trigger="hover">
+            <DatabaseOutlined className="status-icon" />
+        </Popover>
+        <Tooltip placement="top" title="test">
+            <CodeOutlined className="status-icon" />
+        </Tooltip>
         <CoffeeOutlined className="status-icon" />
         <ReadOutlined className="status-icon" />
-        <SyncOutlined className="status-icon" spin />
     </Space>
 );
 
