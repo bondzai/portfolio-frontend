3428f2deb066ec4c64dfd66c0b224ab502f33de5
JamesBond <introbond.dev@gmail.com>
2024-05-12 06:28:36 +0700

    Refactor

--- frontend/src/components/icons/BrandIcon.jsx
+++ frontend/src/components/icons/BrandIcon.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Avatar } from 'antd';
 
-const url = "https://avatars.githubusercontent.com/u/108642001?v=4";
 
 const BrandIcon = () => (
     <div style={{position: 'fixed', left: "5px", top:"5px"}}>
         <Avatar 
             size={40}
             src={
-                <img src={url} alt="avatar" />
+                <img 
+                    src={"https://avatars.githubusercontent.com/u/108642001?v=4"}
+                />
             }
         />
     </div>
