28bc559859e30783c5c6d7d2ce0fd1fe9a9cd709
JamesBond <introbond.dev@gmail.com>
2024-05-12 11:37:57 +0700

    WIP: Use Antd footer

--- frontend/src/pages/global/Footer.jsx
+++ frontend/src/pages/global/Footer.jsx
@@ -1,23 +1,26 @@
 import React from "react";
+import { Layout } from "antd";
 import Counter from "../../components/icons/Counter";
 import Coppyright from "../../components/icons/Copyright";
 import StatusIcons from "../../components/icons/StatusIcons";
 import Watchers from "../../components/icons/Watchers";
 import { Users } from "../../apis/websocket/๊Users";
-import "./Footer.css"
+import "./Footer.css";
 
+
+const { Footer: AntFooter } = Layout;
 
 const Footer = () => {
     const [activeUsersCount] = Users();
 
     return (
-        <div className="footer">
+        <AntFooter className="footer">
             <div className="status-icons">
                 <StatusIcons />
             </div>
             <Coppyright /> &nbsp; <Counter />
             <Watchers activeUsersCount={activeUsersCount} />
-        </div>
+        </AntFooter>
     );
 };
 
