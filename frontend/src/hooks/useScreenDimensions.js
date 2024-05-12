4c995ad1ea9329731016218dec8e4e880355cb84
JamesBond <introbond.dev@gmail.com>
2024-05-12 16:55:08 +0700

    Refactor

--- frontend/src/hooks/useScreenDimensions.js
+++ frontend/src/hooks/useScreenDimensions.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 
+
 export const ScreenSize = {
     XS: "xs",
     SM: "sm",
--- frontend/src/hooks/useScroll.js
+++ frontend/src/hooks/useScroll.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 
+
 const useScrollEffect = () => {
     const [showScrollButton, setShowScrollButton] = useState(false);
     const behavior = "smooth"
