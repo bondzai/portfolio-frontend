ae106d8bafb1ae3d6fa5d0d071cd05b682685359
JamesBond <introbond.dev@gmail.com>
2024-05-12 12:03:27 +0700

    Refactor

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import * as ReactDOM from "react-dom/client";
 import { ApolloProvider } from "@apollo/client";
 import client from "./apis/graphql/apolloClient"
 import App from "./App";
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
