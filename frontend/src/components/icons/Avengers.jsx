6c244e193ab19cdc7a67d2605aa1d747da6860a2
JamesBond <introbond.dev@gmail.com>
2024-05-12 15:46:41 +0700

    Feat: Avengers avatar group

--- frontend/src/components/icons/Avengers.jsx
+++ frontend/src/components/icons/Avengers.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import { Avatar, Tooltip } from 'antd';
 import { openInNewTab } from "../../utils/utils.js";
+import { avengers } from '../../apis/rest/Heroes.js';
 
-
-const Avengers = ({avatar, title, repoUrl, url, description}) => {
-    url = "https://avatars.githubusercontent.com/u/108642001?v=4";
-    repoUrl = url;
+const Avenger = ({ avatar, title, repoUrl, url, description }) => {
     return (
-        <Tooltip title={repoUrl}>
-        <Avatar size={40} src={<img src={url}/>} onClick={() => openInNewTab(repoUrl)} />
-        </Tooltip>
+        <div style={{ cursor: "pointer" }}>
+            <Tooltip title={title}>
+                <Avatar size={40} src={avatar} onClick={() => openInNewTab(repoUrl)} />
+            </Tooltip>
+        </div>
     );
 };
+
+const Avengers = () => {
+    return (
+        <div style={{ display: "flex", marginTop: "30px", marginBottom: "20px" }}>
+            {Object.values(avengers).map((avenger) => (
+                <Avenger 
+                    key={avenger.title} 
+                    avatar={avenger.avatar} 
+                    title={avenger.title} 
+                    repoUrl={avenger.repoUrl} 
+                    url={avenger.url} 
+                    description={avenger.description} 
+                />
+            ))}
+        </div>
+    );
+}
 
 export default Avengers;
