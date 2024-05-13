173e6c9eb8f480d03050c6e78d49423af6bd64b3
JamesBond <introbond.dev@gmail.com>
2024-05-13 11:18:05 +0700

    Refactor: Custom useRoation hook

--- frontend/src/components/Certification.jsx
+++ frontend/src/components/Certification.jsx
@@ -1,30 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
+import useRotation from "../hooks/useRotation";
 
 
 const Certification = ({ ...certification }) => {
+    const { rotation, handleMouseMove, handleMouseLeave } = useRotation();
     const navigate = useNavigate();
+
     const disableClick = window.matchMedia("(max-width: 600px)").matches;
-
-    const [rotation, setRotation] = useState({ x: 0, y: 0 });
-
-    const handleMouseMove = (e) => {
-        const rect = e.target.getBoundingClientRect();
-        const x = e.clientX - (rect.left + rect.width / 2);
-        const y = e.clientY - (rect.top + rect.height / 2);
-
-        setRotation({
-            x: -y / 20,
-            y: x / 20
-        });
-    };
-
-    const handleMouseLeave = () => {
-        setRotation({ x: 0, y: 0 });
-    };
-
     const handleClick = () => {
         if (!disableClick) {
             navigate("/certification/" + certification.id);
