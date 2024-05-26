cee16014faaf0c3379aa23659051506c42d18b5a
James Bond <introbond.dev@gmail.com>
2024-05-26 12:28:47 +0700

    Clean

--- backend/internal/services/google-sheet/sheet_config.go
+++
@@ -1,7 +0,0 @@
-package googlesheet
-
-import "portfolio/utils"
-
-func getGoogleSheetURL() string {
-	return utils.GetEnv("GO_SHEET_URL", "")
-}
--- frontend/src/apis/rest/WakatimeStats.js
+++
@@ -1,9 +0,0 @@
-import { getSingleObject } from "./common/logics.js"
-import { BACKEND_URLS } from "./common/urls.js";
-
-const getWakatimeStats = () => getSingleObject({
-    urls: BACKEND_URLS,
-    endpoint: "/wakatime/",
-})
-
-export { getWakatimeStats }
