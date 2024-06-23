9c59d58aae96416fb80caf1197d7dd4048219f4b
James Bond <introbond.dev@gmail.com>
2024-06-23 13:58:46 +0700

    WIP: Get version

--- src/components/icons/BrandIcon.jsx
+++ src/components/icons/BrandIcon.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Avatar } from 'antd';
-
+import versionData from "../../version.json";
 
 const BrandIcon = () => (
-    <div style={{position: 'fixed', left: "5px", top:"5px"}}>
+    <div style={{position: 'fixed', left: "5px", top:"5px", color: "whitesmoke"}}>
         <Avatar 
             size={40}
-            src={
-                <img 
-                    src={"https://avatars.githubusercontent.com/u/108642001?v=4"}
-                />
-            }
+            src={<img src={"https://avatars.githubusercontent.com/u/108642001?v=4"}/>}
         />
+        <small> V:{versionData.version} </small>
     </div>
 );
 
