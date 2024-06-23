d648de2beac4e1910400fc385afb7a51d8d4b471
James Bond <introbond.dev@gmail.com>
2024-06-23 14:09:43 +0700

    WIP: Style

--- src/components/icons/BrandIcon.jsx
+++ src/components/icons/BrandIcon.jsx
@@ -3,13 +3,17 @@ import { Avatar } from 'antd';
 import versionData from "../../version.json";
 
 const BrandIcon = () => (
-    <div style={{position: 'fixed', left: "5px", top:"5px", color: "whitesmoke"}}>
-        <Avatar 
-            size={40}
-            src={<img src={"https://avatars.githubusercontent.com/u/108642001?v=4"}/>}
-        />
-        <small> V:{versionData.version} </small>
-    </div>
+    <>
+        <div style={{position: 'fixed', left: "3px", top:"5px", color: "whitesmoke"}}>
+            <Avatar 
+                size={40}
+                src={<img src={"https://avatars.githubusercontent.com/u/108642001?v=4"}/>}
+            />
+        </div>
+        <div style={{position: 'fixed', left: "45px", top:"5px", color: "whitesmoke", fontSize: "12px"}}>
+            <small> V:{versionData.version} </small>
+        </div>
+    </>
 );
 
 export default BrandIcon;
