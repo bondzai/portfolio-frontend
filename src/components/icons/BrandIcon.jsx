9c0ea457b6953666369eba948a324d247c125913
James Bond <introbond.dev@gmail.com>
2024-12-30 11:32:22 +0700

    Feat: Read version from pkg.json

--- src/components/icons/BrandIcon.jsx
+++ src/components/icons/BrandIcon.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import { Avatar } from 'antd';
-import versionData from "../../version.json";
+import versionData from "../../../package.json";
 
 const BrandIcon = () => (
     <>
-        <div style={{position: 'fixed', left: "3px", top:"5px", color: "whitesmoke"}}>
-            <Avatar 
-                size={40}
-                src={<img src={"https://avatars.githubusercontent.com/u/108642001?v=4"}/>}
-            />
-        </div>
-        <div style={{position: 'fixed', left: "45px", top:"5px", color: "whitesmoke", fontSize: "12px"}}>
+        <div style={{position: 'fixed', left: "10px", top:"15px", color: "whitesmoke", fontSize: "12px"}}>
             <small> V:{versionData.version} </small>
         </div>
     </>
