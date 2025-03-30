c815a92f3b6cbfe15d5ef850e2020b3aaafec5ac
James Bond <introbond.dev@gmail.com>
2025-03-30 17:59:42 +0700

    Feature: theme control

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 
-const themeOptions = ['light', 'dark'];
+const themeOptions = ['dark', 'light'];
 
 const ThemeContext = createContext();
 
