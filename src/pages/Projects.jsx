157d7b127aded9550e8d7ba65b34de77d1485db9
James Bond <introbond.dev@gmail.com>
2025-04-05 19:24:38 +0700

    Refactor

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,17 +1,14 @@
 import React, { useState, useEffect, useMemo } from "react";
-import { Pagination, Stack } from "@mui/material";
 import Project from "../components/cards/Project";
 import SpinLoader from "../components/loaders/SpinLoader";
 import { getProjectList } from "../apis/rest/Project";
-import { globalDelay, itemsPerPage } from "../utils/constants";
-import useScreenDimensions, { ScreenSize } from "../hooks/useScreenDimensions";
+import { globalDelay } from "../utils/constants";
 import "./Projects.css";
 
 
 const Projects = () => {
     const [projectList, setProjectList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-    const [currentPage, setCurrentPage] = useState(1);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -24,15 +21,6 @@ const Projects = () => {
         fetchData();
     }, []);
 
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const visibleProjects = projectList.slice(startIndex, startIndex + itemsPerPage);
-
-    const handleChangePage = (_, newPage) => {
-        setCurrentPage(newPage);
-    };
-
-    const { screenSize } = useScreenDimensions();
-
     if (isLoading) {
         return (
             <div className="spin-container">
@@ -43,40 +31,13 @@ const Projects = () => {
 
     return (
         <div className="projects" style={{ position: 'relative', minHeight: '85vh' }}>
-            <div className="projectList">
+            <div className="project-list">
                 {
-                    (screenSize === ScreenSize.XS) ? (
-                        projectList.map((project, index) => (
-                            <Project key={index} id={index} {...project} />
-                        ))
-                    ) : (
-                        visibleProjects.map((project, index) => (
-                            <Project key={index} id={index} {...project} />
-                        ))
-                    )
+                    projectList.map((project, index) => (
+                        <Project key={index} id={index} {...project} />
+                    ))
                 }
             </div>
-            {
-                !(screenSize === ScreenSize.XS) && (
-                    <Stack
-                        spacing={2}
-                        justifyContent="center"
-                        style={{ position: 'absolute', bottom: '20px', left: '50%', transform: 'translateX(-50%)' }}
-                    >
-                        <Pagination
-                            count={Math.ceil(projectList.length / itemsPerPage)}
-                            page={currentPage}
-                            onChange={handleChangePage}
-                            sx={{
-                                '& .MuiPaginationItem-root': {
-                                    color: 'var(--text-color-primary)',
-                                    fontWeight: 'bold'
-                                }
-                            }}
-                        />
-                    </Stack>
-                )
-            }
         </div>
     );
 };
