0568a8b48907df8366f708fa4147c2cbdf8f1d84
James Bond <introbond.dev@gmail.com>
2025-12-17 08:41:39 +0700

    Feat: Update home description

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,8 @@ const Home = () => {
                 />
                 <div className="prompt">
                     <h3> Hi, I am James-Bond. </h3>
-                    <p> <strong>  Architect | Engineer | Builder </strong> </p>
+                    <p> <strong>  Architect | Engineer | Entrepreneur </strong> </p>
+                    <p> <strong>  Blockchain Enthusiast </strong> </p>
                     <p> “Clean, simple, & high-quality solution <br /> always sparks joy in my blood.” </p>
                 </div>
                 <Typewriter
